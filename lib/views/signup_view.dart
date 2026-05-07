c471ad02792751e4ff3dac613d1d169b33222e08
lamasiddhu <siddhulama555@gmail.com>
2026-05-07 18:09:25 +0545

    commit

--- lib/views/signup_view.dart
+++ lib/views/signup_view.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
-
-class SignupView extends StatelessWidget {
-  const SignupView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+git commit-m "si"
