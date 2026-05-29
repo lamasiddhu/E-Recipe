610629ca47035820b67d84fb4ef7681936c7542e
lamasiddhu <siddhulama555@gmail.com>
2026-05-29 18:03:23 +0545

    HEADING

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,45 +1,51 @@
-
 import 'package:flutter/material.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
   @override
   State<DashboardView> createState() => _DashboardViewState();
 }
 
-class _DashboardViewState extends State<DashboardView>{
-//background and scroll
-@override
-Widget build(BuildContext context) {
-  const Color bgColor = Color(0xFFF7F2E9);
-  return Scaffold(
-    backgroundColor: bgColor,
-    body: SafeArea(
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-  Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //HEADER
-    children: [
-      const Text(
-        'E-Recipe',
-        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Color(0xFFB84715)),
-      ),
-      CircleAvatar(
-        radius: 18,
-        backgroundColor: Color(0xFFB84715),
-        child: const Icon(Icons.person, size: 20, color: Colors.white),
-      ),
-    ],
-  ),
-  const SizedBox(height: 20),
-],
+class _DashboardViewState extends State<DashboardView> {
+    //BACKGROUND 
+  @override
+  Widget build(BuildContext context) {
+    const Color bgColor = Color(0xFFF7F2E9);
+    const Color brandColor = Color(0xFFB84715);
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // HEADING
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'E-Recipe',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: brandColor,
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 18,
+                    backgroundColor: brandColor,
+                    child: const Icon(Icons.person, size: 20, color: Colors.white),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
